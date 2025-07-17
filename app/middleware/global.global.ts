872c43cd135f645971acefd48c2fbf32f7bae162
Axel.F <axelfertinel@gmail.com>
2025-07-17 16:11:12 +0200

    test des middlewares

---
+++ app/middleware/global.global.ts
@@ -0,0 +1,3 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+    //console.log(`Navigation de ${from.fullPath} vers ${to.fullPath}`);
+});
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,8 +2,4 @@
     <div><h1>Home</h1></div>
 </template>
 
-<script>
-export default {};
-</script>
-
 <style lang="scss" scoped></style>
