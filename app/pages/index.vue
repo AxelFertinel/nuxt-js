9b60e84ba25064a9aaa75982c38fb15bbebd4c25
Axel.F <axelfertinel@gmail.com>
2025-07-21 15:33:16 +0200

    fin du projet petit projet de test

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,5 +2,14 @@
     <div class="container">
         <h1>Accueil</h1>
         <p>Petite application pour créer des tâches et manipuler nuxt.js</p>
+        <ul class="list-disc">
+            <li>Utilisation de Nuxt.js avec vue.js</li>
+            <li>Utilisation de l'orm Prisma avec sqllite</li>
+            <li>Création de point d'api dynamique</li>
+            <li>Création de page static et dynamique</li>
+            <li>Utilisation de fetch pour retrouver les data depuis la DB</li>
+            <li>Utilisation de shadcn pour des élément UX</li>
+            <li>Utilisation de Tailwind css pour le style</li>
+        </ul>
     </div>
 </template>
